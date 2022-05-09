732067fc7aff17cf4f0b0d02729138360cdef7fd
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-05-09 18:57:27 +0100

    app css imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navigation from './components/navBar/Navigation';
+import '../src/styles/App.css'
 
 const App = () => {
     return (
         <div className='container'>
-
+            <Navigation />
         </div>
     )
 }
