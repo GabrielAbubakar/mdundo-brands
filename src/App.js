5c7f1458f156a3ce9430996f5bdaa5af8004b51e
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-05-10 00:39:18 +0100

    home section: initial setup and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navigation from './components/navBar/Navigation';
 import '../src/styles/App.css'
+import BrandLift from '../src/assets/images/BrandLift-Desktop.png'
 
 const App = () => {
     return (
         <div className='container'>
             <Navigation />
+
+            <section className="home">
+                <div className="home__container">
+                    <div className="home__text-content">
+                        <h1>Get Started With Mdundo Brandlift Tool Today</h1>
+                        <p>Increase brand awareness, expand your audience, and boost foot traffic, among other things. Track the impact of your ad and gain insights on delivery and audience engagement.</p>
+                        <a href="#try">Try Now</a>
+                    </div>
+
+                    <div className="home__img-container">
+                        <img src={BrandLift} alt="bradlift desktop " />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
